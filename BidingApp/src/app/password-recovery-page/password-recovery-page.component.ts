b9673c3521bdff99c9a44edf99de96732edb964c
Suriya211 <pksuriyapksuriya@gmail.com>
2024-12-10 11:47:42 +0530

    Initial commit

---
+++ BidingApp/src/app/password-recovery-page/password-recovery-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-recovery-page',
+  imports: [],
+  templateUrl: './password-recovery-page.component.html',
+  styleUrl: './password-recovery-page.component.css'
+})
+export class PasswordRecoveryPageComponent {
+
+}
